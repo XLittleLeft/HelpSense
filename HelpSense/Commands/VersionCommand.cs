f5398d5cf8723378eda21c469cc2312374731f63
永安404 <yongandevelopers@163.com>
2025-01-25 22:33:17 +0800

    i18n

--- HelpSense/Commands/VersionCommand.cs
+++ HelpSense/Commands/VersionCommand.cs
@@ -1,5 +1,6 @@
 ﻿using CommandSystem;
 using HelpSense.API.Features.Pool;
+using HelpSense.ConfigSystem;
 using System;
 
 namespace HelpSense.Commands
@@ -16,31 +17,31 @@ namespace HelpSense.Commands
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
             var config = Plugin.Instance.Config;
-
+            CommandTranslateConfig CommandTranslateConfig = Plugin.Instance.CommandTranslateConfig;
             var sb = StringBuilderPool.Pool.Get();
 
             //Version and update time
-            sb.AppendFormat("当前插件版本为：{0}", Plugin.PluginVersion).AppendLine();
-            sb.AppendFormat("最后更新时间：{0}", Plugin.LastUpdateTime).AppendLine();
-            sb.AppendFormat("推荐游戏版本：{0}", Plugin.RequiredGameVersion).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["PluginVersion"], Plugin.PluginVersion).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["LastUpdateTime"], Plugin.LastUpdateTime).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["RequiredGameVersion"], Plugin.RequiredGameVersion).AppendLine();
 
             //Function list
-            sb.AppendLine("功能开启列表(不是全部.只显示比较重要的):");
-            sb.AppendFormat("非礼勿视机动特遣队:{0}", BoolTranslate(config.EnableSeeNoEvil)).AppendLine();
-            sb.AppendFormat("天网机动特遣队:{0}", BoolTranslate(config.EnableSkynet)).AppendLine();
-            sb.AppendFormat("SCP-023 黑煞星:{0}", BoolTranslate(config.SCP023)).AppendLine();
-            sb.AppendFormat("SCP-029 暗影之女:{0}", BoolTranslate(config.EnableSCP029)).AppendLine();
-            sb.AppendFormat("SCP-073 亚伯/亚当:{0}", BoolTranslate(config.SCP073)).AppendLine();
-            sb.AppendFormat("SCP-191 机械少女:{0}", BoolTranslate(config.SCP191)).AppendLine();
-            sb.AppendFormat("SCP-347 隐形女:{0}", BoolTranslate(config.SCP347)).AppendLine();
-            sb.AppendFormat("SCP-703 壁橱之中:{0}", BoolTranslate(config.EnableSCP703)).AppendLine();
-            sb.AppendFormat("SCP-1056 大小改变器: {0}", BoolTranslate(config.SCP1056)).AppendLine();
-            sb.AppendFormat("SCP-1068 无害核弹:{0}", BoolTranslate(config.SCP1068)).AppendLine();
-            sb.AppendFormat("混沌领导者:{0}", BoolTranslate(config.EnableChaosLeader)).AppendLine();
-            sb.AppendFormat("SCP-2936-1 巨型德国机器人:{0}", BoolTranslate(config.SCP2936)).AppendLine();
-            sb.AppendFormat("SCP-1093 灯人:{0}", BoolTranslate(config.SCP1093)).AppendLine();
-            sb.AppendFormat("无限子弹:{0}", BoolTranslate(config.InfiniteAmmo)).AppendLine();
-            sb.AppendFormat("无限子弹模式:{0}", Plugin.Instance.Config.InfiniteAmmoType).AppendLine();
+            sb.AppendLine(CommandTranslateConfig.VersionCommand["Text"]);
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SeeNoEvil"], BoolTranslate(config.EnableSeeNoEvil)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["Skynet"], BoolTranslate(config.EnableSkynet)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP023"], BoolTranslate(config.SCP023)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP029"], BoolTranslate(config.EnableSCP029)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP073"], BoolTranslate(config.SCP073)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP191"], BoolTranslate(config.SCP191)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP347"], BoolTranslate(config.SCP347)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP703"], BoolTranslate(config.EnableSCP703)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP1056"], BoolTranslate(config.SCP1056)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP1068"], BoolTranslate(config.SCP1068)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["ChaosLeader"], BoolTranslate(config.EnableChaosLeader)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP2936"], BoolTranslate(config.SCP2936)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["SCP1093"], BoolTranslate(config.SCP1093)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["InfiniteAmmo"], BoolTranslate(config.InfiniteAmmo)).AppendLine();
+            sb.AppendFormat(CommandTranslateConfig.VersionCommand["InfiniteAmmoType"], Plugin.Instance.Config.InfiniteAmmoType).AppendLine();
 
             //Copyright
             sb.AppendLine("-Made By X小左(XLittleLeft)-");
--- HelpSense/ConfigSystem/CommandTranslateConfig.cs
+++ HelpSense/ConfigSystem/CommandTranslateConfig.cs
@@ -21,5 +21,39 @@ namespace HelpSense.ConfigSystem
         public string InfoCommandShot { get; set; } = "一共开了<color=red>%shot%</color>枪";
         [Description("玩家信息指令_因DNT查询失败的消息")]
         public string InfoCommandFailed { get; set; } = "查询失败,请关闭DNT或服务器未启用此功能";
+        [Description("卡虚空自救指令_错误")]
+        public string RescueCommandError { get; set; } = "执行指令时发生错误，请稍后再试";
+        [Description("卡虚空自救指令_失败")]
+        public string RescueCommandFailed { get; set; } = "失败，可能指令未启用或者身份不允许等";
+        [Description("卡虚空自救指令_成功")]
+        public string RescueCommandOk { get; set; } = "成功";
+        [Description("聊天指令_错误")]
+        public string ChatCommandError { get; set; } = "发送消息时出现错误，请稍后重试";
+        [Description("聊天指令_失败")]
+        public string ChatCommandFailed { get; set; } = "发送失败，你被禁言或者信息为空或者聊天系统未启用";
+        [Description("聊天指令_成功")]
+        public string ChatCommandOk { get; set; } = "发送成功";
+        [Description("查询HelpSense插件版本和信息")] 
+        public Dictionary<string,string> VersionCommand { get; set; } = new() { 
+            { "PluginVersion", "当前插件版本为：{0}" },
+            { "LastUpdateTime", "最后更新时间：{0}" }, 
+            { "RequiredGameVersion", "推荐游戏版本：{0}" },
+            { "Text", "功能开启列表(不是全部.只显示比较重要的):" },
+            { "SeeNoEvil", "非礼勿视机动特遣队:{0}" },
+            { "Skynet", "天网机动特遣队:{0}" },
+            { "SCP023", "SCP-023 黑煞星:{0}" },
+            { "SCP029", "SCP-029 暗影之女:{0}" },
+            { "SCP073", "SCP-073 亚伯/亚当:{0}" },
+            { "SCP191", "SCP-191 机械少女:{0}" },
+            { "SCP347", "SCP-347 隐形女:{0}" },
+            { "SCP703", "SCP-703 壁橱之中:{0}" },
+            { "SCP1056", "SCP-1056 大小改变器: {0}" },
+            { "SCP1068", "SCP-1068 无害核弹:{0}" },
+            { "ChaosLeader", "混沌领导者:{0}" },
+            { "SCP2936", "SCP-2936-1 巨型德国机器人:{0}" },
+            { "SCP1093", "SCP-1093 灯人:{0}" },
+            { "InfiniteAmmo", "无限子弹:{0}" },
+            { "InfiniteAmmoType", "无限子弹模式:{0}" }
+        };   
     }
 }
